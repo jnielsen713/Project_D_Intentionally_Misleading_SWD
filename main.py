4083f0a783b22194c00078e0e2a9d438e2a66300
jnielsen713 <143571582+jnielsen713@users.noreply.github.com>
2025-04-15 15:00:01 -0700

    Added bare py files

---
+++ main.py
@@ -0,0 +1,5 @@
+# Imports --------------------------------------------------------------------------------------------------------------
+from dash import Dash
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.graph_objects as go
